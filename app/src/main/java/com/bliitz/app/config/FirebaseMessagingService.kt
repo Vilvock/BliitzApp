fd97bd38f67389c9dc69cdc736ee1d7c4982e372
Vilvock <luisvilvock@gmail.com>
2023-03-22 14:43:51 -0300

    neutral

--- app/src/main/java/com/bliitz/app/config/FirebaseMessagingService.kt
+++ app/src/main/java/com/bliitz/app/config/FirebaseMessagingService.kt
@@ -11,6 +11,7 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.bliitz.app.R
+import com.bliitz.app.main_ui.activity.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
