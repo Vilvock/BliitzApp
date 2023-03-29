dd693fc0dcc0f097d2a95c977f99061d70a53e38
Vilvock <luisvilvock@gmail.com>
2023-03-29 16:52:34 -0300

    primeiro

--- app/src/main/java/com/bliitz/app/main_ui/fragment/menu/home/ProductDetailFragment.kt
+++ app/src/main/java/com/bliitz/app/main_ui/fragment/menu/home/ProductDetailFragment.kt
@@ -11,7 +11,11 @@ import com.bliitz.app.R
 import com.bliitz.app.global_ui.components.CircleRecyclerViewDecoration
 import com.bliitz.app.global_ui.config_fragment.BaseFragment
 import com.bliitz.app.main_ui.adapter.CarrouselProductItemAdapter
+import com.bliitz.app.main_ui.adapter.CategoryAdapter
+import com.bliitz.app.util.RecyclerItemListener
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_product_detail.*
+import kotlinx.android.synthetic.main.fragment_product_detail.categories_rv
 
 /**
  * A simple [Fragment] subclass.
@@ -37,6 +41,31 @@ class ProductDetailFragment : BaseFragment() {
 
         loadCarrousel()
 
+        val list = ArrayList<Any>()
+
+        list.add(Any())
+        list.add(Any())
+        list.add(Any())
+        list.add(Any())
+        list.add(Any())
+
+        val categoryAdapter = CategoryAdapter(requireActivity(), list, object : RecyclerItemListener {
+            override fun onClickListenerItem(item: Any?) {
+                super.onClickListenerItem(item)
+
+            }
+
+        })
+        val layoutManagerRvHorizontal: RecyclerView.LayoutManager = (object : LinearLayoutManager(requireContext(), HORIZONTAL, false) {
+            override fun checkLayoutParams(lp: RecyclerView.LayoutParams): Boolean {
+                // force height of viewHolder here, this will override layout_height from xml
+//                lp.width = (width / 2.6).toInt()
+                return true
+            }
+        })
+
+        categories_rv.layoutManager = layoutManagerRvHorizontal
+        categories_rv.adapter = categoryAdapter
     }
 
 
